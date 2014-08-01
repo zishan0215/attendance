6bd34216a4f6ed8689e59d30cf6c2336e42d4094
Zishan Ahmad <zishanahmad@outlook.com>
2014-08-01 17:40:27 +0530

    Loading the form helper and validation from teacher_controller

--- application/libraries/teacher_controller.php
+++ application/libraries/teacher_controller.php
@@ -4,6 +4,9 @@ class Teacher_Controller extends MY_Controller
 	function __construct() {
 		parent::__construct();
 		$this->data['meta_title'] = 'Attendance Management System';
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+
 	}
 }
 
