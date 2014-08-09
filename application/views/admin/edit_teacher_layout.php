f28943ab6dbf92113b374b382951caacf1195e90
unknown <nkmittal4994@gmail.com>
2014-08-09 13:54:48 +0530

    edit teacher option

--- application/views/admin/edit_teacher_layout.php
+++ application/views/admin/edit_teacher_layout.php
@@ -2,9 +2,12 @@
 	<div class="row">
 		<!-- Main column -->
 		<div class="col-md-9">
-			<h2>Edit teacher: Put teacher name here</h2>
+			<h2>Edit teacher details of : <? php ?></h2>
 			<h4>Teacher Id: <?php echo $teacher_id; //remove this when done with the editing ?></h4>
 			<br>
+			<section>
+				
+			</section>
 			<section>
 				<!-- Put the form below this line -->
 				<form method="post" action="http://localhost/jmiams/index.php/admin/edit_teacher">
@@ -15,9 +18,38 @@
 					    	<input type="text" class="form-control" name="teacher_name" value="Put name here through PHP" required>
 					    </div>
 					</div>
+					<br>
 					<!-- Insert other fields here -->
-
-
+					<div class="form-group">
+						    <label for="username" class="col-sm-3 control-label">Username</label>
+						    <div class="col-sm-7">
+						    	<input type="text" class="form-control" placeholder="Username for login" name="username" required>
+						    </div>
+						</div>
+						<div class="form-group">
+						    <label for="password" class="col-sm-3 control-label">Password</label>
+						    <div class="col-sm-7">
+						    	<input type="text" class="form-control" placeholder="Password for login" name="password" required>
+						    </div>
+						</div>
+						<div class="form-group">
+						    <label for="subjectname" class="col-sm-3 control-label">Subject Name</label>
+						    <div class="col-sm-7">
+						    	<input type="text" class="form-control" placeholder="Name of the subject" name="subject_name" required>
+						    </div>
+						</div>
+						<div class="form-group">
+						    <label for="subjectcode" class="col-sm-3 control-label">Subject Code</label>
+						    <div class="col-sm-7">
+						    	<input type="text" class="form-control" placeholder="Subject Code (Eg. CEN-501)" name="subject_code" required>
+						    </div>
+						</div>
+						<div class="form-group">
+						    <label for="semester" class="col-sm-3 control-label">Semester</label>
+						    <div class="col-sm-7">
+						    	<input type="text" class="form-control" placeholder="Subject semester" name="semester" required>
+						    </div>
+						</div>
 
 					<div class="form-group">
 					    <div class="col-sm-offset-3 col-sm-7">
