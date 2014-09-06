0ff0ac234705001bfdda123ea533f07cb675c56e
unknown <nkmittal4994@gmail.com>
2014-09-06 12:47:23 +0530

    js check for new period

--- application/views/admin/new_period_layout.php
+++ application/views/admin/new_period_layout.php
@@ -37,7 +37,7 @@
 						<input type="text" id="fromdate" class="form-control datepicker" name="from_date" placeholder="Enter from date (yyyy-mm-dd)" required>
                         <br>
                         <label for="todate">To Date</label>
-						<input type="text" id="todate" class="form-control datepicker" name="to_date" placeholder="Enter to date (yyyy-mm-dd)" onchange="validate_new_period();" required>
+						<input type="text" id="todate" class="form-control datepicker" name="to_date" placeholder="Enter to date (yyyy-mm-dd)" onblur="validate_new_period();"  required>
   						</div>
   						<br>
   						<input type="submit" name="submit" value="Submit" class="btn btn-primary">&nbsp;&nbsp;
--- js/main.js
+++ js/main.js
@@ -13,9 +13,45 @@ function validate_total_classes() {
 	}
 }
 function validate_new_period() {
-	var start = document.getElementById('fromdate').value;
-	var end = document.getElementById('todate').value;
-	if(start > end) {
+	var start = document.getElementById('fromdate').value.split('-');
+	var end = document.getElementById('todate').value.split('-');
+	var start_d = parseInt(start[2]);
+  	var start_m  = parseInt(start[1]);
+  	var start_y = parseInt(start[0]);
+  	var end_d = parseInt(end[2]);
+  	var end_m  = parseInt(end[1]);
+  	var end_y = parseInt(end[0]);
+	if(start_y > end_y) {
+		$('#div_dates').addClass('has-error');
+		alert('From date of period cannot exceed the to date in the time period.')
+		$('#todate').val("");
+	} else if(start_y == end_y) {
+		if(start_m > end_m) {
+			$('#div_dates').addClass('has-error');
+			alert('From date of period cannot exceed the to date in the time period.')
+			$('#todate').val("");
+		} else if(start_m == end_m) {
+			if(start_d > end_d) {
+				$('#div_dates').addClass('has-error');
+				alert('From date of period cannot exceed the to date in the time period.')
+				$('#todate').val("");
+			} else {
+				if($('#div_dates').hasClass('has-error')) {
+					$('#div_dates').removeClass('has-error');
+				}
+			}
+
+		} else {
+			if($('#div_dates').hasClass('has-error')) {
+			$('#div_dates').removeClass('has-error');
+			}
+		}
+	} else {
+		if($('#div_dates').hasClass('has-error')) {
+			$('#div_dates').removeClass('has-error');
+		}
+	}
+	/*if(start > end) {
 		$('#div_dates').addClass('has-error');
 		alert('From date of period cannot exceed the to date in the time period.')
 		$('#todate').val("");
@@ -23,5 +59,5 @@ function validate_new_period() {
 		if($('#div_dates').hasClass('has-error')) {
 			$('#div_dates').removeClass('has-error');
 		}
-	}
+	}*/
 }
