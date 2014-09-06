89ecb286378aea90e2888dbaede8cffd9099330a
unknown <nkmittal4994@gmail.com>
2014-09-07 01:28:06 +0530

    check:attendance of student shud be less than total classes

--- js/main.js
+++ js/main.js
@@ -26,13 +26,13 @@ function validate_new_period() {
 	}
 }
 
-function validate_attendance() {
+function validate_attendance(present_id) {
 	var total = document.getElementById('total_classes').value;
-	var input = document.getElementById('total').value;
+	var input = document.getElementById(present_id).value;
 	if(input > total) {
 		$('#div_total').addClass('has-error');
 		alert('Attendance cannot exceed the total classes.')
-		$('#total').val("");
+		document.getElementById(present_id).value = '';
 	} else if (input <= total) {
 		if($('#div_total').hasClass('has-error')) {
 			$('#div_total').removeClass('has-error');
